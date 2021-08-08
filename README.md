537f7d1d6577f1be38bfdef3f80aabad94996d89
Jeff <jmunozcastro@protonmail.com>
2021-08-08 16:25:04 -0600

    principal functions done

---
+++ README.md
@@ -0,0 +1,32 @@
+[![JeffreyMC](https://img.shields.io/badge/JeffreyMC-Developer-red)](https://github.com/JeffreyMC)
+[![Licencia](https://img.shields.io/github/license/JeffreyMC/PagoConMonedas)](https://github.com/JeffreyMC/PagoConMonedas/blob/main/LICENSE)
+[![Twitter Follow](https://img.shields.io/twitter/follow/JeffreyMC16?style=social)](https://twitter.com/JeffreyMC16)
+
+#  Árbol binario
+
+## Funciones
+
+* Indicar cantidad de nodos
+* Insertar Nodos
+* Imprimir lista de nodos en preOrden
+* Imprimir lista de nodos en inOrden
+* imprimir lista de nodos en postOrden
+* imprimir la altura del árbol
+
+## Detalles
+
+* Lenguaje Java
+* JDK 8
+* Netbeans 8.2
+* Windows 10
+
+## Capturas
+
+### Menú
+
+![Menu](menu.png)
+
+![Funcionamiento](app.png)
+
+
+
---
+++ src/BinaryTree/Funciones.java
@@ -0,0 +1,130 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BinaryTree;
+
+/**
+ *
+ * @author Jeffrey
+ */
+public class Funciones {
+
+ 
+    //NODO RAÍZ
+     Nodo root;
+     
+     //variables String
+     private String listaPre = " ";
+     private String listaIn = " ";
+     private String listaPost = " ";
+     
+     
+    //GETS Y SETS DE LOS ATRIBUTOS 
+    public String getListaPre() {
+        return listaPre;
+    }
+    
+        /**
+     * @param listaPre the listaPre to set
+     */
+    public void setListaPre(String listaPre) {
+        this.listaPre = listaPre;
+    }
+
+    /**
+     * @param listaIn the listaIn to set
+     */
+    public void setListaIn(String listaIn) {
+        this.listaIn = listaIn;
+    }
+
+    /**
+     * @param listaPost the listaPost to set
+     */
+    public void setListaPost(String listaPost) {
+        this.listaPost = listaPost;
+    }
+    
+    /**
+     * @return the listaIn
+     */
+    public String getListaIn() {
+        return listaIn;
+    }
+
+    /**
+     * @return the listaPost
+     */
+    public String getListaPost() {
+        return listaPost;
+    }
+    
+    //inserta un nodo
+    public void add(int valor){
+        root = addRecursivo(root, valor);
+    }
+    
+    //método recursivo
+    private Nodo addRecursivo(Nodo actual, int valor){
+        if(actual == null){
+            return new Nodo(valor);
+        }
+        
+        if(valor < actual.valor){
+            actual.izquierda = addRecursivo(actual.izquierda, valor);
+        }
+        else if(valor > actual.valor){
+            actual.derecha = addRecursivo(actual.derecha, valor);
+        }else{
+            return actual;
+        }
+        
+        return actual;
+    }
+    
+    //imprime nodo
+    public void preOrden(Nodo nodo){
+        if(nodo != null){
+            setListaPre(listaPre + nodo.valor + " - ");
+            preOrden(nodo.izquierda);
+            preOrden(nodo.derecha);
+        }
+    }
+    
+    public void postOrden(Nodo nodo){
+        if(nodo != null){
+            postOrden(nodo.izquierda);
+            postOrden(nodo.derecha);
+            setListaPost(listaPost + nodo.valor + " - ");
+        }
+    }
+    
+    public void inOrden(Nodo nodo){
+        if(nodo != null){
+            inOrden(nodo.izquierda);
+            setListaIn(listaIn + nodo.valor + " - ");
+            inOrden(nodo.derecha);
+        }
+    }
+    
+    //muestra la altura del árbol
+    public int alturaArbol(Nodo raiz) {
+        if (raiz == null) {
+            return 0;
+        }
+        
+        //llama a la función de manera recursiva y encuentra
+        //la altura de la rama derecha y la rama izquierda del árbol
+        int alturaIzquierda = alturaArbol(raiz.izquierda);
+        int alturaDerecha = alturaArbol(raiz.derecha);
+        //verifica la altura de la derecha u la izquierda
+        if (alturaDerecha > alturaIzquierda) {
+            return 1 + alturaDerecha;
+        } else {
+            return 1 + alturaIzquierda;
+        }
+}
+   
+}
